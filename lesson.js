685081d18500a371031aecbc6e73db2edb9acab9
Erobor Favour <108184061+erobor@users.noreply.github.com>
2023-06-01 11:38:03 +0100

    Add files via upload

---
+++ lesson.js
@@ -0,0 +1,37 @@
+
+var sunday = "7th day";
+
+console.log('Day before yesterday is the ', sunday)
+
+
+let a = 45;
+let b = 50;
+let c = 'the summation of a and b equals'
+console.log( c, a + b)
+
+// conditional statement: if statement, case statement
+
+
+// if(){
+
+// }
+
+// ===, !=, <, >.
+
+// ------------------------statement---------------------
+
+let gender = "man";
+
+// ------------------------condition---------------------
+
+if (gender === "male") {
+    console.log("Faith is  a male")
+
+} else if(gender === "female"){
+    console.log("Faith is a female")
+
+} else {
+    console.log("She's none of it")
+}
+
+
